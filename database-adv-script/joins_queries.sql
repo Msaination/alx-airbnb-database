1917ad0e230e3d82876018f500d72bad58f0b9de
Sipho Nzilwane <64207098+Msaination@users.noreply.github.com>
2025-10-30 19:44:58 +0200

    Add SQL query to join bookings and users

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,1 +1,12 @@
+SELECT 
+    bookings.booking_id,
+    bookings.property_id,
+    bookings.booking_date,
+    users.user_id,
+    users.name,
+    users.email
+FROM 
+    bookings
+INNER JOIN 
+    users ON bookings.user_id = users.user_id;
 
